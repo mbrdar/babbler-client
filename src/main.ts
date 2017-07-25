022d68a99dddbb8efeb377ef3e19805c1b23d683
milos.brdar <milos.brdar@codecentric.de>
2017-07-25 16:29:51 +0200

    Change module name and version

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { BabblerClient } from './app/app.module';
+import { BabblerClient } from './app/babbler-client.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
